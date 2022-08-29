4ac0b4703756d2f67b0ba951a632631b488cd5cf
andmassbell <andremassenabelo@gmail.com>
2022-08-29 16:33:14 -0300

    exercicios-js

---
+++ meus-exercicios/exercicio-2/index2.js
@@ -0,0 +1,14 @@
+const pergunta1 = ("Você teme a morte?")
+const resposta1 = prompt(pergunta1)
+const pergunta2 = ("Você teme o frio?")
+const resposta2 = prompt(pergunta2)
+const pergunta3 = ("Você teme o calor?")
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, resposta1)
+console.log(pergunta2, resposta2)
+console.log(pergunta3, resposta3)
+
+
+
+
